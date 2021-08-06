e6d637abd45d6c02e111cdab4bcbb04e14a8148e
ELANGO RAJ <elangorj@gmail.com>
2021-08-06 18:41:30 +0100

    Update create_data_lists.py
    Path update

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,7 +1,7 @@
 from utils import create_data_lists
 
 if __name__ == '__main__':
-    create_data_lists(train_folders=['/Dataset/train/full_960x720'],
-                      test_folders=['/Dataset/test/England_960x720'],
+    create_data_lists(train_folders=['Dataset/train/full_960x720'],
+                      test_folders=['Dataset/test/England_960x720'],
                       min_size=100,
-                      output_folder='/Dataset')
+                      output_folder='Dataset/')
